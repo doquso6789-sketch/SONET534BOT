955af2f0a32920f8966151df1a103529aff96552
doquso <doquso6789@gmail.com>
2026-05-26 00:34:57 +0700

    Update start.py

--- handlers/start.py
+++ handlers/start.py
@@ -1,1 +1,17 @@
+from telegram import Update
+from telegram.ext import ContextTypes
 
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+
+    text = (
+        "🔥 SOCIAL DOWNLOADER BOT\n\n"
+        "✅ TikTok No Watermark\n"
+        "✅ Facebook\n"
+        "✅ Instagram\n"
+        "✅ YouTube\n"
+        "✅ SoundCloud\n\n"
+        "📥 Send Link"
+    )
+
+    await update.message.reply_text(text)
