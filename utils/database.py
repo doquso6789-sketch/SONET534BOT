7fd0014358640cc3779357f5d887accf8c4a9b07
doquso <doquso6789@gmail.com>
2026-05-26 00:28:42 +0700

    Update database.py

--- utils/database.py
+++ utils/database.py
@@ -1,1 +1,21 @@
+import json
+import os
 
+DB_FILE = "data/users.json"
+
+os.makedirs("data", exist_ok=True)
+
+if not os.path.exists(DB_FILE):
+    with open(DB_FILE, "w") as f:
+        json.dump([], f)
+
+
+def save_user(user_id):
+    with open(DB_FILE, "r") as f:
+        users = json.load(f)
+
+    if user_id not in users:
+        users.append(user_id)
+
+    with open(DB_FILE, "w") as f:
+        json.dump(users, f)
